011109c9641e34cc6c58a9b28e465bce1bdc321b
QanjuQ <anjumqureshi000@gmail.com>
2019-04-11 11:41:58 +0530

    Draw rectangles with stars

--- src/patterns/core.clj
+++ src/patterns/core.clj
@@ -1,6 +1,38 @@
 (ns patterns.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def reversed-repeat (comp (partial apply repeat) reverse list))
+
+(def solid-line (partial reversed-repeat "*"))
+
+(def space-line (partial reversed-repeat " "))
+
+(def dec-two (comp dec dec))
+
+(def append (comp
+             (partial apply concat)
+             (juxt rest (comp list first))
+             cons))
+
+(def join-with-newline  (partial mapcat (partial append "\n")))
+
+(defn repeat-twice-less [count what-to] (repeat (dec-two count) what-to))
+
+(def hollow-star-line
+  (comp
+   (partial append "*")
+   (partial cons "*")
+   space-line
+   dec-two))
+
+(def inner-body
+  (comp
+   (partial map hollow-star-line)
+   repeat-twice-less))
+
+(defn hollow-rectangle [length breadth]
+  (comp (append (solid-line breadth)
+           (cons (solid-line breadth) (inner-body length breadth)))))
+
+(defn render [shape length width] (print (shape length width)))
+
+(def solid-rectangle (comp (partial map solid-line) repeat))
--- test/patterns/core_test.clj
+++ test/patterns/core_test.clj
@@ -2,6 +2,50 @@
   (:require [clojure.test :refer :all]
             [patterns.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest solid-rectangle-test
+  (testing "A solid rectangle with length and width 4,5 respectively"
+    (is (= '(("*" "*" "*" "*" "*")
+             ("*" "*" "*" "*" "*")
+             ("*" "*" "*" "*" "*")
+             ("*" "*" "*" "*" "*"))
+           (solid-rectangle 4 5))))
+  
+  (testing "A solid rectangle with length and width 3,2 respectively"
+    (is (= '(("*" "*" "*")
+             ("*" "*" "*"))
+           (solid-rectangle 3 2)))))
+
+(deftest hollow-rectangle-test
+  (testing "A hollow rectangle with length and width 4,5 respectively"
+    (is (= '(("*" "*" "*" "*" "*")
+             ("*" " " " " " " "*")
+             ("*" " " " " " " "*")
+             ("*" "*" "*" "*" "*"))
+           (hollow-rectangle 4 5))))
+  
+  (testing "A solid rectangle with length and width 3,2 respectively"
+    (is (= '(("*" "*" "*")
+             ("*" "*" "*"))
+           (hollow-rectangle 3 2)))))
+
+(deftest left-triangle-test 
+  (testing "A triangle of width and height 6"
+    (is (= '(("*")
+             ("*" "*")
+             ("*" "*" "*")
+             ("*" "*" "*" "*")
+             ("*" "*" "*" "*" "*")
+             ("*" "*" "*" "*" "*" "*"))
+             (left-triangle 6))))
+  (testing "A triangle of width and height 3"
+    (is (= '(("*")
+             ("*" "*")
+             ("*" "*" "*"))
+             (left-triangle 6)))))
+
+(deftest right-triangle-test
+  (testing "A triangle of width and height 6"
+    (is (= '((" " " " "*")
+             (" " "*" "*")
+             ("*" "*" "*"))
+             (right-triangle 4)))))
